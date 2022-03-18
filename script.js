30e9f6b6ab8d1b9a7bfc9c225886d3379cd14d19
Payalchede <99233336+Payalchede@users.noreply.github.com>
2022-03-18 18:28:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var Toilet=[["1","2","3"],["7"]]
+console.log( Toilet)
+
+var Toilet=[[[4,5,6],[9,10,11]],[[8],[15]]]
+console.log(Toilet)
+
+var Toilet=[[[12,13,14],[17,18,19]],[[16],[23]]]
+console.log(Toilet)
+
+var Toilet=[[[20,21,22],[25,26,27]],[[24],[31]]]
+console.log(Toilet)
+
+var Toilet=[[[28,29,30],[33,34,35]],[[32],[39]]]
+console.log(Toilet)
+
+var Toilet=[[[36,37,38],[41,42,43]],[[40],[47]]]
+console.log(Toilet)
+
+var Toilet=[[[44,45,46],[49,50,51]],[[48],[49]]]
+console.log(Toilet)
+
+var Toilet=[[[52,53,54],[57,58,59]],[[56],[63]]]
+console.log(Toilet)
+
+var Toilet=[[[60,61,62],[65,66,67]],[[64],[71]]]
+console.log(Toilet)
+
+var Toilet=[["68","69","70"],["72"]]
+console.log( Toilet)
+
+
+var num=[54] && [7]
+console.log("Seat Number:54.\n Row Number:7")
+if (num){
+console.log("Upper Berth")
+}
+else{
+    console.log("other")
+}
